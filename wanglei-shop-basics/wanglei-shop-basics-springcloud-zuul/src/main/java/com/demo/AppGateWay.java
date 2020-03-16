54e0005539a9a5fa92a34c926e436c26fb0610b2
wl <768543036@qq.com>
2020-03-16 16:34:45 +0800

    阿波罗配置中心整合

--- wanglei-shop-basics/wanglei-shop-basics-springcloud-zuul/src/main/java/com/demo/AppGateWay.java
+++ wanglei-shop-basics/wanglei-shop-basics-springcloud-zuul/src/main/java/com/demo/AppGateWay.java
@@ -1,5 +1,10 @@
 package com.demo;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,7 +22,12 @@ import java.util.List;
 @EnableSwagger2Doc
 @EnableEurekaClient
 @EnableZuulProxy
+@EnableApolloConfig
 public class AppGateWay {
+
+    @ApolloConfig
+    private Config appConfig;
+
     public static void main(String[] args) {
         SpringApplication.run(AppGateWay.class,args);
     }
@@ -28,12 +38,39 @@ public class AppGateWay {
 
         @Override
         public List<SwaggerResource> get() {
-            List resources = new ArrayList();
-            resources.add(swaggerResources("app-mayikt-member","/app-mayikt-member/v2/api-docs","2.0"));
-            resources.add(swaggerResources("app-mayikt-weixin","/app-mayikt-weixin/v2/api-docs","2.0"));
+//            List resources = new ArrayList();
+//            resources.add(swaggerResources("app-mayikt-member","/app-mayikt-member/v2/api-docs","2.0"));
+//            resources.add(swaggerResources("app-mayikt-weixin","/app-mayikt-weixin/v2/api-docs","2.0"));
+//            return resources;
+            return resources();
+        }
+
+        private List<SwaggerResource> resources() {
+
+            List resources = new ArrayList<>();
+            // app-itmayiedu-order
+            // 网关使用服务别名获取远程服务的SwaggerApi
+            String swaggerDocJson = swaggerDocument();
+            JSONArray jsonArray = JSONArray.parseArray(swaggerDocJson);
+            for (Object object : jsonArray) {
+                JSONObject jsonObject = (JSONObject) object;
+                String name = jsonObject.getString("name");
+                String location = jsonObject.getString("location");
+                String version = jsonObject.getString("version");
+                resources.add(swaggerResources(name, location, version));
+            }
             return resources;
         }
 
+        /**
+         * 获取swaggerDocument配置
+         *
+         * @return
+         */
+        private String swaggerDocument() {
+            String property = appConfig.getProperty("wanglei-swagger-resources", "");
+            return property;
+        }
         private SwaggerResource swaggerResources(String name,String location,String version) {
             SwaggerResource swaggerResource = new SwaggerResource();
             swaggerResource.setName(name);
